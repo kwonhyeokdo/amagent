872b6a8e89c03771b64057167f7ed0caea8d6060
kwonhd <kwonhd>
2024-11-18 13:42:44 +0900

    test

--- src/pages/Worktime.jsx
+++ src/pages/Worktime.jsx
@@ -1,9 +1,25 @@
 import { Stack, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 
 const Worktime = ()=>{
     return (
         <Stack>
             <Typography>{new Date().getMonth() + 1}월 근로 시간</Typography>
+            <DataGrid
+                rows={[]}
+                columns={[
+                    { field: 'date', headerName: '일자', width: 90 },
+                    { field: 'workType', headerName: '구분', width: 90 },
+                    { field: 'startTime', headerName: '업무시작', width: 90 },
+                    { field: 'endTime', headerName: '업무종료', width: 90 },
+                    { field: 'restTime', headerName: '기본휴게시간', width: 90 },
+                    { field: 'bonusTime', headerName: '추가휴게시간', width: 90 },
+                    { field: 'overtime', headerName: '업무재개', width: 90 },
+                    { field: 'worktime', headerName: '일 근무시간', width: 90 },
+                ]}
+                checkboxSelection
+                disableRowSelectionOnClick
+            />
         </Stack>
     );
 }
